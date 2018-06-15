fb9cc7c860c995562f39bd91868922ba4c1d63ee
AntonNoga <noga.anton@pdffiller.team>
2018-06-15 19:33:23 +0300

    Task-1 solution

--- task-1/index.js
+++ task-1/index.js
@@ -6,9 +6,25 @@
  */
 function fillSpiralMatrix(n) {
   const result = [[]];
+  for (var i=0;i<n;i++){result[i]=[]};
+  var c=1; // data to insert
+  var i=0;
+  var j=0;
 
-  // Ваш код
+  // lev уровень погружения в матрицу. 0 - внешний круг начинаем проход с (0,0), 1 - начинаем с (1,1) 
+  for (var lev=0; lev<n/2;lev++) 
+    { 
+    i=lev;
+    for (j=lev;j<n-lev-1;j++) {result[i][j]=c; c++;};
+    for (i=lev;i<n-lev-1;i++) {result[i][j]=c; c++;};
+    for (j=n-lev-1;j>lev;j--) {result[i][j]=c; c++;};
+    for (i=n-lev-1;i>lev;i--) {result[i][j]=c; c++;};
+    }
+    lev--;
+    // если разменость нечетное доставляем последний элемент в центр
+    if (n%2!=0) {result[lev][lev]=c;}
 
+  
   return result;
 }
 
