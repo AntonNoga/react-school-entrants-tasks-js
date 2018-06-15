b733d52a4d9b1591d7fa31af92c073f3fed73b65
AntonNoga <noga.anton@pdffiller.team>
2018-06-15 19:53:21 +0300

    Task-2 not full solution

--- task-2/index.js
+++ task-2/index.js
@@ -8,6 +8,15 @@
  * @returns {Boolean}
  */
 function haveSameItems(arr1, arr2) {
+
+  if (arr1.length!=arr2.length) return false; 
+  var arr2_sort=arr2;
+  arr2_sort.sort(function(a, b){return a-b});
+  for (var i=0; i<arr1.length;i++) {
+    if (arr1[i]!=arr2_sort[i]) return false;
+  }
+
+
   return true;
 }
 
